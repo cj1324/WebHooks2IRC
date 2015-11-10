1c738439e8d3d30d5c2f7586e6ca796e64e28873
Han Chen <chen1324+git@gmail.com>
2015-11-10 09:52:50 +0800

    add service debug log.

--- src/webhooks2irc/core/ircbot.py
+++ src/webhooks2irc/core/ircbot.py
@@ -3,16 +3,19 @@
 
 import time
 import threading
+import logging
 
 from irc import client as irc_client
 
 from webhooks2irc import settings
+logger = logging.getLogger(__name__)
 
 
 class IrcBotService(threading.Thread):
     def __init__(self):
         self.client = irc_client.Reactor()
         server = self.client.server()
+        logger.debug('connect to {0}.'.format(settings.IRC_HOST))
         server.connect(settings.IRC_HOST,
                        settings.IRC_PORT,
                        settings.IRC_NICK)
@@ -24,9 +27,11 @@ class IrcBotService(threading.Thread):
     def run(self):
         while True:
             if not self.server.is_connected():
+                logger.debug('server reconnect.')
                 self.server.reconnect()
             self.client.process_once(settings.IRC_DATA_TIMEOUT)
             if self.hi.is_set():
+                logger.debug('ready send hi message.')
                 self.server.join("#{0}".format(settings.IRC_CHANNEL))
                 self.server.privmsg("#{0}".format(settings.IRC_CHANNEL),
                                     settings.IRC_HIMSG)
