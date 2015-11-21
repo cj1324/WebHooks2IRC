1acdd4c109c27b987e78e99b183c8c74013c10cc
Han Chen <chen1324+git@gmail.com>
2015-11-21 17:39:08 +0800

    added put_message testcase for ircbot.

--- tests/test_ircbot.py
+++ tests/test_ircbot.py
@@ -23,6 +23,16 @@ class IrcBotServiceTestCase(unittest.TestCase):
         time.sleep(1)
         self.assertTrue(self.ibs.is_alive())
 
+    def test_bot_put_miss_obj_fail(self):
+        self.ibs.put_message({})
+        time.sleep(1)
+        self.assertFalse(self.ibs.is_alive())
+
+    def test_bot_put(self):
+        self.ibs.put_message({'channel': 'default', 'message': 'testing.'})
+        time.sleep(1)
+        self.assertTrue(self.ibs.is_alive())
+
     def tearDown(self):
         self.ibs.stop.set()
         time.sleep(1)
